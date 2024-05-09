fb4735e106428495d8a4b91b5b0d994a7d705400
Nikita Khmelevskiy <sxxdav@gmail.com>
2024-05-09 23:23:50 +0200

    add debouncer

---
+++ src/shared/lib.ts
@@ -0,0 +1,13 @@
+export function debounce<F extends (...args: any[]) => void>(
+  fn: F,
+  ms: number = 300
+) { 
+  let timeout: ReturnType<typeof setTimeout> | undefined = undefined
+
+ return (...args: Parameters<F>) => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      fn(...args)
+    }, ms)
+  }
+}
